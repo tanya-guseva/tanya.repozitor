c2d9a244eb8b79d4a33e31b601009dbfd36d791f
tanya-guseva <tanya.guseva.vt12@gmail.com>
2014-11-24 20:33:22 +0200

    Update_exercise
    Signed-off-by: tanya-guseva <tanya.guseva.vt12@gmail.com>

--- exercise.php
+++ exercise.php
@@ -1,26 +1,28 @@
 <?php
-	var N = 10;
+	var N;
+  var i;
 	var A[] = array();
-    var k = 0;
-    var sum = 0;
+  var k = 0;
+  var sum = 0;
 
+  get(N);
 
-    for(i=1;i<=N;i++) {
+  for(i=1;i<=N;i++) {
     	get(A[i]);
+  }
+  i = 1;
+  while (i <= N) do {
+    if((A[i] mod 2) == 0) {
+    	sum = sum + A[i];
+    	k++;
     }
-    i = 1;
-    while (i < N) do {
-    	if((A[i] mod 2) == 0) {
-    		sum = sum + A[i];
-    		k++;
-    	}
-    	i = i + 2;
-    } 
-   if(k != 0) {
-   		sum / k;
-   		print(sum / k);
-   }
-   else {
+    i = i + 2;
+  } 
+  if(k != 0) {
+   	sum / k;
+   	print(sum / k);
+  }
+  else {
    		print("Нет четных элементов, стоящих на нечетных местах");
-   }
+  }
 ?>
