dfb22cea4a962a56c05c5b5b39d37ee07424efd1
tanya-guseva <tanya.guseva.vt12@gmail.com>
2014-11-30 09:04:14 +0200

    Update_exercise(new version)
    Signed-off-by: tanya-guseva <tanya.guseva.vt12@gmail.com>

--- exercise.php
+++ exercise.php
@@ -1,28 +1,34 @@
 <?php
-	var N;
-  var i;
+/*Получить среднее арифметическое всех чётных элементов массива,
+стоящих на нечётных местах*/
+  var str="";
+  var p;
 	var A[] = array();
   var k = 0;
   var sum = 0;
 
-  get(N);
-
-  for(i=1;i<=N;i++) {
+  for(i=1;i<=A.count();i++) {
+    if(get(str)=="stop"){
+      break;
+    }
+    else{
     	get(A[i]);
+    }  
   }
-  i = 1;
-  while (i <= N) do {
-    if((A[i] mod 2) == 0) {
-    	sum = sum + A[i];
+  p = 1;
+  while (p <= A.count()) do {
+    if((A[p] mod 2) == 0) {
+    	sum = sum + A[p];
     	k++;
     }
-    i = i + 2;
+    p = p + 2;
   } 
   if(k != 0) {
-   	sum / k;
    	print(sum / k);
   }
   else {
    		print("Нет четных элементов, стоящих на нечетных местах");
   }
 ?>
+
+
